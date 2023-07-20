d1b3f7537a1d7431c2bf274f409b974c2d71995d
blloop <42186044+blloop@users.noreply.github.com>
2023-07-19 23:17:10 -0700

    add menu screen

--- script.js
+++ script.js
@@ -43,16 +43,12 @@ function checkCombat() {
     if (a && collide(p2, a)) {
       p2.takeHit(p1.damage);
       a.hurt = false;
-      // document.getElementById('health-2').style.width = 
-      //   `${p2.health}%`;
     }
   });
   attacks2.forEach((a) => {
     if (a && collide(p1, a)) {
       p1.takeHit(p2.damage);
       a.hurt = false;
-      // document.getElementById('health-1').style.width = 
-      //   `${p1.health}%`;
     }
   });
 }
@@ -63,15 +59,31 @@ function checkGame() {
   if (p1.health > 0 && p2.health > 0 && timer > 0) {
     return;
   }
+  timer = 0;
   gameStatus = p1.health > p2.health ?
     'Player 1 Wins!' : (p2.health > p1.health ?
       'Player 2 Wins!' : '   Tie Game   ');
 }
 
-// Box dimensions (X, Y, width, height)
-let box1 = [30, 40, 400, 30];
-let box2 = [530, 40, 400, 30];
-let box3 = [430, 10, 100, 100];
+// In game timer
+let timer;
+let gameTimer;
+function startTimer(count) {
+  timer = count;
+  updateTimer();
+}
+function updateTimer() {
+  if (timer > 0) {
+    gameTimer = setTimeout(updateTimer, 1000);
+    timer -= 1;
+  }
+}
+
+// Draw status bars (health, timer, result)
+// Box dimensions: [X, Y, width, height]
+let box1 = [30, 40, 400, 30]; // p1 health bar
+let box2 = [530, 40, 400, 30]; // p2 health bar
+let box3 = [430, 10, 100, 100]; // timer
 function drawStatus() {
   c.fillStyle = 'red';
   c.fillRect(box1[0], box1[1], box1[2], box1[3]);
@@ -82,7 +94,9 @@ function drawStatus() {
     c.fillRect(320, 145, 320, 80);
   }
   c.fillStyle = 'yellow';
-  c.fillRect(box1[0] + (4 * (100 - p1.health)), box1[1], box1[2] - (4 * (100 - p1.health)), box1[3]);
+  c.fillRect(box1[0] + (4 * (100 - p1.health)), box1[1], 
+    box1[2] - (4 * (100 - p1.health)), box1[3]
+  );
   c.fillRect(box2[0], box2[1], box2[2] - ( 4 * (100 - p2.health)), box2[3]);
   c.font = '40px Verdana';
   c.fillStyle = 'white';
@@ -92,31 +106,43 @@ function drawStatus() {
   }
 }
 
-// In game timer
-let timer = 31;
-let gameTimer;
-function updateTimer() {
-  if (timer > 0) {
-    gameTimer = setTimeout(updateTimer, 1000);
-    timer -= 1;
-  }
+// Draw main menu
+// Box dimensions: [X, Y, width, height]
+let menu = 0;
+let box4 = [380, 280, 200, 80, false]; // Play button
+function drawMenu() {
+  c.fillStyle = 'grey';
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.fillStyle = box4[4] ? 'green' : 'lightgreen';
+  c.fillRect(box4[0], box4[1], box4[2], box4[3]);
+  c.fillStyle = 'black';
+  c.font = '40px Verdana';
+  c.fillText('PLAY', box4[0] + 45, box4[1] + 55);
+  c.fillText('DINO BRAWL', 340, 200);
 }
 
 // Event loop function
 function loop() {
+  switch (menu) {
+    case 0: // Main menu
+      drawMenu();
+      break;
+    case 1: // Game
+      bg.update();
+      checkCombat();
+      checkGame();
+      drawStatus();
+      p1.update();
+      p2.update();
+      attacks1.forEach((a) => a ? a.update() : void(0));
+      attacks2.forEach((a) => a ? a.update() : void(0)); 
+  }
   window.requestAnimationFrame(loop);
-  bg.update();
-  checkCombat();
-  checkGame();
-  drawStatus();
-  p1.update();
-  p2.update();
-  attacks1.forEach((a) => a ? a.update() : void(0));
-  attacks2.forEach((a) => a ? a.update() : void(0));
 }
 
 loop();
-updateTimer();
+
+// Event listeners for keyboard + mouse input
 
 window.addEventListener('keydown', (event) => {
   keys[event.key] = true;
@@ -125,3 +151,31 @@ window.addEventListener('keydown', (event) => {
 window.addEventListener('keyup', (event) => {
   keys[event.key] = false;
 });
+
+// Canvas relative mouse position: 
+// https://stackoverflow.com/questions/17130395
+window.onmousemove = function(e) {
+  let rect = canvas.getBoundingClientRect();
+  let mX = e.pageX - rect.left;
+  let mY = e.pageY - rect.top;
+  if (mX > box4[0] && mY > box4[1] && 
+    mX < box4[0] + box4[2] && mY < box4[1] + box4[3]) {
+    canvas.style.cursor = 'pointer';
+    box4[4] = true;
+  } else {
+    canvas.style.cursor = 'default';
+    box4[4] = false;
+  }
+}
+
+window.onmouseup = function(e) {
+  let rect = canvas.getBoundingClientRect();
+  let mX = e.pageX - rect.left;
+  let mY = e.pageY - rect.top;
+  if (mX > box4[0] && mY > box4[1] && 
+    mX < box4[0] + box4[2] && mY < box4[1] + box4[3]) {
+    // Start game
+    menu = 1;
+    startTimer(31);
+  }
+}
